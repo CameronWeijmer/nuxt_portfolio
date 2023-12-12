4da0cb2a40ab7bec90702d64a6ff2af2de267fe7
Cameron Weijmer <weijmer.c@gmail.com>
2023-12-12 22:15:38 +0100

    feat: added probs to page

--- pages/service.vue
+++ pages/service.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
-        <ServiceCards />
+        <div class="py-24 sm:py-32">
+            <ServiceCards title="Webseiten-Design und Entwicklung" description="Ich unterstütze Sie von der ersten Idee bis zur fertigen Webseite, 
+            mit einem ansprechenden Design und reibungsloser technischer Umsetzung. Das Ergebnis 
+            ist eine beeindruckende Webseite, die gut aussieht und einwandfrei funktioniert."
+                desc1="Präzises Webdesign" desc2="Technische Expertise" desc3="Langfristige Unterstützung"
+                desc4="Anpassung an Ihre Ideen" payment="Preis auf Anfrage" heading="Einmal zahlen, immer besitzen." />
+
+            <ServiceCards title="Webseiten-Design und Entwicklung" description="Ich unterstütze Sie von der ersten Idee bis zur fertigen Webseite, 
+            mit einem ansprechenden Design und reibungsloser technischer Umsetzung. Das Ergebnis 
+            ist eine beeindruckende Webseite, die gut aussieht und einwandfrei funktioniert."
+                desc1="Präzises Webdesign" desc2="Technische Expertise" desc3="Langfristige Unterstützung"
+                desc4="Anpassung an Ihre Ideen" payment="Preis auf Anfrage" heading="Einmal zahlen, immer besitzen." />
+        </div>
     </div>
 </template>
 
