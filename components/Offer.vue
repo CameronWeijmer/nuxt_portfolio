6cc3cae86867219ad02b2645f9f4d88cd5e278f7
Cameron Weijmer <weijmer.c@gmail.com>
2023-12-17 19:05:00 +0100

    feat: added offer comp

--- components/Offer.vue
+++ components/Offer.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <a href="kontakt.html#form" class="mt-10 block w-full rounded-md bg-primary px-3 py-2 text-center 
+        <NuxtLink href="kontakt.html#form" class="mt-10 block w-full rounded-md bg-primary px-3 py-2 text-center 
             text-sm font-semibold text-white shadow-sm hover:opacity-80 focus-visible:outline 
             focus-visible:outline-2 focus-visible:outline-offset-2 
-            focus-visible:outline-indigo-600">OfferteAnfordern</a>
+            focus-visible:outline-indigo-600">Offerte Anfordern</NuxtLink>
     </div>
 </template>
 
