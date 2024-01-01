7cc12764a721578cee4bcae1f12a6a3a50f4f041
Cameron Weijmer <weijmer.c@gmail.com>
2024-01-01 19:18:23 +0100

    feat: created contact page

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-
+        <div>
+            <Landing title="kontakt" desc1="Gerne stehe ich Ihnen zur Verfügung, um Ihre Anfragen,"
+                desc2="Offerten und Fragen entgegenzunehmen." />
+        </div>
+        <div>
+            <!-- <Form /> -->
+            <ContactForm imageSrc="/img/email_02.svg" />
+        </div>
     </div>
 </template>
 
