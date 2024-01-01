59733cec01fbf8b23e7839feb5668667509f853b
Cameron Weijmer <weijmer.c@gmail.com>
2024-01-01 18:32:06 +0100

    feat: added project page

---
+++ components/Projects.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <div class="flex justify-evenly flex-wrap">
+
+            <div class="max-w-md md:max-w-2xl bg-white border border-gray-200 rounded-lg shadow-2xl my-20"
+                data-aos="fade-up" data-aos-duration="1000">
+                <div class="md:pt-0 pt-5 flex justify-center relative">
+                    <a :href="link" target="_blank" class="group">
+                        <img class="w-full max-w-md group-hover:opacity-80" :src="source" alt="4clout Projekt">
+                        <div
+                            class="absolute inset-0 flex items-center justify-center opacity-0
+                            group-hover:opacity-80 transition-opacity duration-700 bg-4clout bg-opacity-100 text-white">
+                            <span class="text-xl">Website Ansehen</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="p-5">
+                    <a class="hover:opacity-75" :href="link" target="_blank">
+                        <h2 :class="textColor" class="text-4xl md:text-5xl tracking-tighter font-major">{{ projectTitle }}
+                        </h2>
+                    </a>
+                    <p class="mt-5 font-normal text-gray-700">{{ description }}</p>
+
+                    <div class="flex justify-start items-center space-x-3 pt-5 cursor-default" data-aos="fade"
+                        data-aos-duration="1000">
+                        <svg :class="textColor" class="w-8 h-8" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg class="w-8 h-8 text-4clout" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg class="w-8 h-8 text-4clout" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg class="w-8 h-8 text-4clout" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg class="w-8 h-8 text-4clout" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+const probs = defineProps({
+    link: String,
+    source: String,
+    projectTitle: String,
+    description: String,
+    textColor: String,
+})
+</script>
+
+<style scoped></style>
--- pages/projekte.vue
+++ pages/projekte.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
+        <Landing title="meine projekte" desc1="Gerne präsentiere ich Ihnen eine breite Palette von"
+            desc2="Projekten, an denen ich gearbeitet habe." />
+        <Heading title="Projekte im Überblick" text="In der Übersicht finden Sie eine Auswahl von Projekten, an denen ich aktiv 
+                  beteiligt war. Diese Projekte spiegeln meine breite Expertise und meine Praxiserfahrung 
+                  in verschiedenen Bereichen und Branchen wider." />
+        <Projects link="https://4clout.ch" source="/img/4clout_mac_rbg.png" textColor="text-4clout" projectTitle="4clout"
+            description="4clout KLG als Social Media Management bietet ein Full-Service-Angebot an. Vom Konzept über die Content Production und Bewirtschaftung verschiedener Social Media Plattformen inkl. Community Management." />
+
+        <Projects link="https://javiergarcia.ch/" source="/img/javier_garcia_rbg.png" projectTitle="javier garcia"
+            description="Javier Garcia ist eine bekannte Persönlichkeit, die in verschiedenen Fernsehsendungen wie Comedy aus dem Labor, Stand-Up! und StandUpMigranten aufgetreten ist." />
+
+        <Projects link="https://cameronweijmer.github.io/Nail-Works/" source="/img/nailworks_mac_rbg.png"
+            projectTitle="nailworks" description="Perfekte Nägel, exzellenter Service. Entdecken Sie ihre professionelle Maniküre, Pediküre und mehr in entspannter Atmosphäre. Verwöhnen Sie sich und gönnen Sie Ihren Nägeln die Aufmerksamkeit, die sie verdienen.
+
+" />
 
     </div>
 </template>
 
 <script setup>
-
+definePageMeta({
+    layout: "navigation"
+})
 </script>
 
 <style scoped></style>
